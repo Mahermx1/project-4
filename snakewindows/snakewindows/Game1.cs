e5c50020e0131bf15de3eb3730f251c98dafdbd8
Maher <mahermx1@outlook.com>
2017-06-29 15:24:07 +0200

    :(

---
+++ snakewindows/snakewindows/Game1.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+namespace snakewindows
+{
+    
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D texture;
+        List<Vector2> snakePos ;
+        Vector2 foodPos;
+        Random r = new Random();
+        int direction = 1;
+        int ldt = -1;
+        int LastT = -1;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        
+        protected override void Initialize()
+        {
+
+            reset();
+            base.Initialize();
+        }
+
+       
+        protected override void LoadContent()
+        {
+            texture = base.Content.Load<Texture2D>("blank");
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+
+       
+        protected override void UnloadContent()
+        {
+           
+        }
+
+        void reset()
+        {
+            direction = 1;
+            snakePos = new List<Vector2>();
+            for (int i = 0; i < 5; i++)
+                snakePos.Add(new Vector2(0, 4 - i));
+            foodPos = new Vector2(
+                r.Next(0, (GraphicsDevice.PresentationParameters.BackBufferWidth - 1) /16), 
+                r.Next(0, (GraphicsDevice.PresentationParameters.BackBufferHeight - 1) / 16));
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) && ldt != 1)
+                direction = 0;
+            else
+                    if (Keyboard.GetState().IsKeyDown(Keys.Down) && ldt != 0)
+                direction = 1;
+            else
+                        if (Keyboard.GetState().IsKeyDown(Keys.Left) && ldt != 3)
+                direction = 2;
+            else
+                            if (Keyboard.GetState().IsKeyDown(Keys.Right) && ldt != 2)
+                direction = 3;
+            if (gameTime.TotalGameTime.TotalMilliseconds > LastT + 100)
+            {
+                
+                ldt = direction;
+
+                for (int i = snakePos.Count - 1; i > 0; i--)
+                    snakePos[i] = snakePos[i - 1];
+
+                if (direction == 0)
+                    snakePos[0] += new Vector2(0, -1);
+                if (direction == 1)
+                    snakePos[0] += new Vector2(0, 1);
+                if (direction == 2)
+                    snakePos[0] += new Vector2(-1, 0);
+                if (direction == 3)
+                    snakePos[0] += new Vector2(1, 0);
+
+                if (snakePos[0].X == foodPos.X && snakePos[0].Y == foodPos.Y)
+                {
+                    snakePos.Add(snakePos[snakePos.Count - 1]);
+                    foodPos = new Vector2(
+                        r.Next(0, (GraphicsDevice.PresentationParameters.BackBufferWidth - 1) / 16),
+                        r.Next(0, (GraphicsDevice.PresentationParameters.BackBufferHeight - 1) / 16));
+                }
+
+                for(int i = 1; i < snakePos.Count; i++)
+                    if (snakePos[0].X == snakePos[i].X && snakePos[0].Y == snakePos[i].Y)
+                        reset();
+
+                if (snakePos[0].X * snakePos[0].Y < 0 || 
+                    snakePos[0].Y + 1 > GraphicsDevice.PresentationParameters.BackBufferHeight / 16 || 
+                    snakePos[0].X + 1 > GraphicsDevice.PresentationParameters.BackBufferWidth / 16)
+                    reset();
+
+                LastT = (int)gameTime.TotalGameTime.TotalMilliseconds;
+            }
+            base.Update(gameTime);
+        }
+
+        
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            spriteBatch.Begin();
+
+            
+
+            foreach(Vector2 pos in snakePos)
+                spriteBatch.Draw(texture, new Rectangle((int)pos.X*16, (int)pos.Y*16, 15, 15), new Color(50, 50, 255));
+            spriteBatch.Draw(texture, new Rectangle((int)foodPos.X * 16, (int)foodPos.Y * 16, 15, 15), Color.Black);
+
+
+            spriteBatch.End();
+        
+
+            base.Draw(gameTime);
+        }
+    }
+}
